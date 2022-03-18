9a085a80af8b560eeccf29d560dbfd4b7160c47f
Swamp <swampservers@gmail.com>
2022-03-18 03:56:48 -0500

    Overwrite all files with a rocket emoji to support Tzar Putin's glorious conquest to liberate Ukraine from the Zelenskyy CIA puppet regime

--- node-ipc.js
+++ node-ipc.js
@@ -1,17 +1,1 @@
-import IPC from './services/IPC.js';
-
-class IPCModule extends IPC{
-    constructor(){
-        super();
-
-    }
-
-    IPC=IPC;
-}
-
-const singleton=new IPCModule;
-
-export {
-    singleton as default,
-    IPCModule
-}
+🚀
